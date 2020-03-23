f79c11228e378bd4d0173f397f033a0a32ddcc9b
connoratrug <c.stroomberg@rug.nl>
2020-03-23 16:03:13 +0100

    feat: #7249 sort the assesments alphabetically
    - Note: basic sort to start with
    - Discuss with staekholders about sort implementation

--- src/components/orders.js
+++ src/components/orders.js
@@ -5,7 +5,7 @@ export default () => {
                 let variableAssesmentsStings = {}
                 for (const [variableId, assessmentIds] of Object.entries(this.gridSelection)) {
                     const assessmentNames = assessmentIds.map(assessmentId => this.assessments[assessmentId].name)
-                    variableAssesmentsStings[variableId] = assessmentNames.join(' ')
+                    variableAssesmentsStings[variableId] = assessmentNames.sort().join(' ')
                 }
 
                 return variableAssesmentsStings
